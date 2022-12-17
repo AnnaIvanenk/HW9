050a1417e678026a53f969cb345aaa05bf702771
AnnaIvanenk <anchouze@mail.ru>
2022-12-17 16:46:54 +0300

    ДЗ 9 сдаю

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int m = 1;
+int n = 5;
+Console.WriteLine(SumMN(m - 1, n));
+
+
+// сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
